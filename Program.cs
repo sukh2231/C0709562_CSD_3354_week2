88fb2588c1cbbe39948dd622dadf4ba2183aab36
Sukhraj singh <46693375+sukh2231@users.noreply.github.com>
2019-01-17 17:07:13 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0709562
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dogs = new List<string>();
+
+            dogs.Add("Bulldog");
+            dogs.Add("Poodle");
+            dogs.Add("Retriever");
+
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine(dog + "");
+            }
+        }
+    }
+}
